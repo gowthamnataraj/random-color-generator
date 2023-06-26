beff2601858336dd1fbf822f3b33f721692aa7c0
gowtham <gowthamnatarajan04.com>
2023-06-26 21:56:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Random Color Palette Generator - Adobe Illustrator Extension
+
+The Random Color Palette Generator is an Adobe Illustrator extension that allows users to effortlessly generate random color palettes and design styles for their creative projects. This README file provides an overview of the extension's functionality, instructions for installation, and additional details to help you get started.
+
+## Functionality
+
+The Random Color Palette Generator extension offers the following key features:
+
+- Generate a Random Color Palette: The extension generates a random color palette consisting of five colors. Each color is represented as a circle in the user interface, providing a visual representation of the palette.
+
+- Generate a Random Design Style or Theme: Users can generate a random design style or theme for their projects. The available design styles include vintage, futuristic, minimalistic, abstract, modern, elegant, hand-drawn, geometric, grunge, typography-based, watercolor, and more.
+
+- Customize and Save Themes: Users can customize the generated themes by modifying the color palette and design style according to their preferences. The extension allows them to save the customized themes for future use, making it easy to access and apply them to different projects.
+
+## Installation
+
+To install the Random Color Palette Generator extension in Adobe Illustrator, follow these steps:
+
+1. Clone or download the repository to your local machine.
+2. Open Adobe Illustrator.
+3. In the menu bar, go to **Window > Extensions**.
+4. Click on the **Browse** button and navigate to the location where you saved the extension files.
+5. Select the extension file (RandomColorPaletteGenerator.aip or similar) and click **Open**.
+6. The extension will be installed and appear in the Extensions panel within Adobe Illustrator.
+
+## Usage
+
+To use the Random Color Palette Generator extension, follow these steps:
+
+1. Open Adobe Illustrator.
+2. In the menu bar, go to **Window > Extensions > Random Color Palette Generator**.
+3. The extension interface will open, displaying a random color palette and design style.
+4. To generate a new random color palette, click the **Generate Color** button.
+5. To generate a new random design style, click the **Generate Design** button.
+6. Customize the color palette and design style as desired.
+7. To save the current theme, enter a name in the "Theme Name" input field and click the **Save Theme** button.
+8. Saved themes will appear in the "Saved Themes" section below, displaying the theme name, color palette, design style, and a "Delete Theme" button to remove a saved theme.
+9. To search for specific themes, enter a search term in the "Search" input field. The filtered themes will be displayed below.
+10. Explore and utilize the saved themes in your creative projects.
+
+## Tech Stack
+
+The Random Color Palette Generator extension is built using the following technologies:
+
+- React: A JavaScript library for building user interfaces.
+- TypeScript: A statically-typed superset of JavaScript that compiles to plain JavaScript, providing enhanced developer tooling and improved code maintainability.
+
+## Code Structure
+
+The code for the Random Color Palette Generator extension is structured as follows:
+
+- **App.tsx:** The main component that defines the user interface and handles the extension's functionality.
+- **App.css:** The CSS styles associated with the extension's user interface.
+- **index.tsx:** The entry point of the extension, responsible for rendering the App component.
+
+## Contributing
+
+Contributions to the Random Color Palette Generator extension are welcome! If you have any ideas, suggestions, or bug reports, please open an issue or submit a pull request.
+
+## License
+
+This extension is released under the [MIT License](LICENSE).
+
---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  const [colorPalette, setColorPalette] = useState<string[]>([]);
+  const [designStyle, setDesignStyle] = useState<string>('');
+  const [savedThemes, setSavedThemes] = useState<{ name: string; colors: string[]; style: string }[]>([]);
+  const [themeName, setThemeName] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [filteredThemes, setFilteredThemes] = useState<{ name: string; colors: string[]; style: string }[]>([]);
+
+  useEffect(() => {
+    const savedThemesFromStorage = localStorage.getItem('savedThemes');
+    if (savedThemesFromStorage) {
+      setSavedThemes(JSON.parse(savedThemesFromStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    const filtered = savedThemes.filter(theme => theme.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    setFilteredThemes(filtered);
+  }, [searchTerm, savedThemes]);
+
+  const generateColorPalette = () => {
+    const colors: string[] = [];
+    for (let i = 0; i < 5; i++) {
+      colors.push(generateRandomColor());
+    }
+    setColorPalette(colors);
+  };
+
+  const generateRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const generateDesignStyle = () => {
+    const designStyles = [
+      'Vintage',
+      'Futuristic',
+      'Minimalistic',
+      'Abstract',
+      'Modern',
+      'Elegant',
+      'Hand-drawn',
+      'Geometric',
+      'Grunge',
+      'Typography-based',
+      'Watercolor',
+      // Add more design styles here
+    ];
+    const randomStyle =
+      designStyles[Math.floor(Math.random() * designStyles.length)];
+    setDesignStyle(randomStyle);
+  };
+
+  const saveTheme = () => {
+    const newTheme = { name: themeName, colors: colorPalette, style: designStyle };
+    const updatedThemes = [...savedThemes, newTheme];
+    setSavedThemes(updatedThemes);
+    localStorage.setItem('savedThemes', JSON.stringify(updatedThemes));
+    setThemeName('');
+    console.log('Theme saved!');
+  };
+
+  const deleteTheme = (themeName: string) => {
+    const updatedThemes = savedThemes.filter(theme => theme.name !== themeName);
+    setSavedThemes(updatedThemes);
+    localStorage.setItem('savedThemes', JSON.stringify(updatedThemes));
+    console.log(`Theme "${themeName}" deleted!`);
+  };
+
+  return (
+    <div className="App">
+      <h1>Random Color Palette Generator</h1>
+      <div className="color-palette">
+        {colorPalette.map((color, index) => (
+          <div
+            key={index}
+            className="color"
+            style={{ backgroundColor: color }}
+          />
+        ))}
+      </div>
+      <div className="generate-buttons">
+        <button onClick={generateColorPalette}>Generate Color</button>
+        <button onClick={generateDesignStyle}>Generate Design</button>
+      </div>
+
+      {designStyle && (
+        <p>
+          Random Design: <strong>{designStyle}</strong>
+        </p>
+      )}
+      <div className="save-theme">
+        <input
+          type="text"
+          placeholder="Theme Name"
+          value={themeName}
+          onChange={(e) => setThemeName(e.target.value)}
+        />
+        <button onClick={saveTheme}>Save Theme</button>
+      </div>
+      <div className="search-themes">
+        <h2>Search Themes</h2>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="saved-themes">
+        <h2>Saved Themes</h2>
+        {filteredThemes.map((theme, index) => (
+          <div className="theme-card" key={index}>
+            <h3>{theme.name}</h3>
+            <div className="color-palette">
+              {theme.colors.map((color, index) => (
+                <div
+                  key={index}
+                  className="color"
+                  style={{ backgroundColor: color }}
+                />
+              ))}
+            </div>
+            <p>Design: {theme.style}</p>
+            <button onClick={() => deleteTheme(theme.name)}>Delete Theme</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
